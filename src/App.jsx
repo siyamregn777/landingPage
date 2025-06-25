702f759d450dd946abf7cd7f08c7d10818b72046
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-06-25 11:03:14 +0300

    refactor: restructure App component layout and improve section organization

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-
-
+import {Hero,PopularProducts,SuperQuality,Services,SpecialOffers,CustomerReviews,Subscribe,Footer} from "./sections/index.js"
+import Nav from "./components/Nav.jsx";
 const App = () => (
   <main className="relative">
-    <section className="xl:p-1 wide:pr-4 pb-4 bg-blue-500">
-      hero
+    <Nav/>
+    <section className="xl:padding-l wide:padding-r padding-b">
+      <Hero/>
+    </section>
+    <section className="padding">
+      <PopularProducts/>
+    </section>
+    <section className="padding">
+      <SuperQuality/>
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffers/>
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews/>
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe/>
+    </section><section className="padding-x padding-t pb-8 bg-black">
+      <Footer/>
     </section>
   </main>
 );
+
 export default App;
